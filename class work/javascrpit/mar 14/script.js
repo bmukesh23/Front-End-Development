1fd7e187c372d08d16819e07aa3720af2f9b36f6
Mukesh <billamukesh1234@gmail.com>
2023-03-15 22:30:11 +0530

    tues

--- class work/javascrpit/mar 14/script.js
+++ class work/javascrpit/mar 14/script.js
@@ -2,14 +2,20 @@
 
 const arr = [2341232, '123212332', 23, '   12332   ', 234123212332324]
 
-const masked = arr => {
+const masked = carr => {
 
-    for(let n of arr){
-        const srr = String(n).trim();
-        let k = srr.slice(-4);
-        const a = k.padStart(srr.length,'*');
-        // console.log(a);
+    for(let n of carr.entries()){
+        const index = n[0];
+        let indexArr = n[1];
+
+        indexArr = String(indexArr).trim();
+        let k = indexArr.slice(-4);
+
+        const a = k.padStart(indexArr.length, '*').padEnd(20, ' ');
+        const locks = k.length >= 4 ? '🔒'.repeat(index + 1): '';
+
+        console.log(a, locks);
     }
-}
+};
 
 masked(arr);
