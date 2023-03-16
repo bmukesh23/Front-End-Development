ebf2335c974b2e908c93e8943c3844aa010256d9
Mukesh <billamukesh1234@gmail.com>
2023-03-16 18:26:30 +0530

    tues

---
+++ class work/javascrpit/mar 15/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const weekDays = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'];
+
+//The restaurant "xyz" on "x-weekday" opens at "open" and closes at "close".
+
+const restaurant = {
+     openingHours : {
+        mon : {
+          open : 9,
+          close : 17
+        },
+
+        wed : {
+
+        },
+
+        fri : {
+
+        }
+
+     }
+
+} = weekDays;
