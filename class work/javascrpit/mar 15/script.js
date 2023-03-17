106960a1864145b40b0cd9fe390ba75a29635765
Mukesh <billamukesh1234@gmail.com>
2023-03-17 20:55:51 +0530

    tues

--- class work/javascrpit/mar 15/script.js
+++ class work/javascrpit/mar 15/script.js
@@ -2,23 +2,37 @@
 
 const weekDays = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'];
 
-//The restaurant "xyz" on "x-weekday" opens at "open" and closes at "close".
-
 const restaurant = {
-     openingHours : {
-        mon : {
-          open : 9,
-          close : 17
-        },
+  name: "Random",
+  openingHours: {
+    [weekDays[1]]: {
+      open: 9,
+      close: 17
+    },
 
-        wed : {
+    [weekDays[3]]: {
+      open: 13,
+      close: 8
+    },
 
-        },
+    [weekDays[5]]: {
+      open: 14,
+      close: 0
+    }
 
-        fri : {
+  }
 
-        }
+};
 
-     }
+console.log(restaurant);
 
-} = weekDays;
+const openingHrsEntries = Object.entries(restaurant.openingHours);
+for (const x of openingHrsEntries) {
+  // const weekday = x[0];
+  // const openTime = x[1].open;
+  // const closeTime = x[1].close;
+
+  const [weekday, { open: openTime, close: closeTime }] = x
+
+  console.log(`The restuarant ${restaurant.name} on ${weekday} ${weekday === 'wed' ? 'nes' : ''}day opens at ${openTime} and closes at ${closeTime}.`)
+}
