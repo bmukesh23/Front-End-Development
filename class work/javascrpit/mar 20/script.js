0e28db7a78daefd41d7490f7f2e5ff06343491cd
Mukesh <billamukesh1234@gmail.com>
2023-03-20 23:44:10 +0530

    tues

---
+++ class work/javascrpit/mar 20/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+'use strict';
+
+const weekDays = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'];
+
+const restaurant = {
+  name: "Random",
+  openingHours: {
+    [weekDays[1]]: {
+      open: 9,
+      close: 17
+    },
+
+    [weekDays[3]]: {
+      open: 13,
+      close: 8
+    },
+
+    [weekDays[5]]: {
+      open: 14,
+      close: 0
+    }
+
+  }
+
+};
+
+// const openingHrsEntries = Object.entries(restaurant.openingHours);
+// for (const x of openingHrsEntries) {
+//   const [weekday, { open: openTime, close: closeTime }] = x
+
+//   console.log(`The restuarant ${restaurant.name} on ${weekday} ${weekday === 'wed' ? 'nes' : ''}day opens at ${openTime} and closes at ${closeTime}.`)
+// }
