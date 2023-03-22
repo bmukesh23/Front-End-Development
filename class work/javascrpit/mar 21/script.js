4aa4c1b96947c83b7661622d8be2f2b147d2d40e
Mukesh <billamukesh1234@gmail.com>
2023-03-22 22:54:53 +0530

    tues

--- class work/javascrpit/mar 21/script.js
+++ class work/javascrpit/mar 21/script.js
@@ -5,15 +5,12 @@ const str = "Random sentence with multiple words";
 const makeOneWord = str1 => {
     console.log(str1.replaceAll(' ', ''));
 }
-
 makeOneWord(str);
 
-
 const makeFirstWordUpperCase = str2 => {
-    const array = str2.split(' ');
-    for (let i = 1; i < 5; i++) {
-        console.log(array[0].toUpperCase(), ...array);
-    }
+    let string = str2.split(' ');
+    string = string[0].toUpperCase() + string.slice(1);
+    console.log(string);
 }
 
 makeFirstWordUpperCase(str);
