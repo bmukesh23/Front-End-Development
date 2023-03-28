114206773cd0e83d0eebfad5e19b60b07c6a0a1c
Mukesh <billamukesh1234@gmail.com>
2023-03-28 22:57:41 +0530

    tues

--- class work/javascrpit/mar 21/script.js
+++ class work/javascrpit/mar 21/script.js
@@ -6,7 +6,7 @@ const makeOneWord = str1 => {
     console.log(str1.replaceAll(' ', ''));
 }
 makeOneWord(str);
-
+mujkesrygc
 const makeFirstWordUpperCase = str2 => {
     let string = str2.split(' ');
     string = string[0].toUpperCase() + ' ' + (string.slice(1).join(' '));
