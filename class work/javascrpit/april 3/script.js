8388cfbc329112e1be5cf9f93638c22e668d718a
Mukesh <billamukesh1234@gmail.com>
2023-04-15 22:52:57 +0530

    tues

--- class work/javascrpit/april 3/script.js
+++ class work/javascrpit/april 3/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const users = [
+    { firstName: 'Random', age: 26 },
+    { firstName: 'Piyush', age: 14 },
+    { firstName: 'Kunal', age: 22 },
+    { firstName: 'Raju', age: 16 },
+];
+
+// Log the firstname of all the users
+
+users.forEach(function(user){
+    console.log(user.firstName);
+});
